3df375887c5f1d8d0df19be1512749574aa55732
AnderLX <andrew1230@gmail.com>
2016-03-10 13:59:08 +0800

    list controller

--- Application/Starball/Controller/ListController.class.php
+++ Application/Starball/Controller/ListController.class.php
@@ -5,4 +5,9 @@ class ListController extends Controller {
     public function index(){
     	$this->display();
     }
+	
+	public function byTimeRange($range, $page=1){
+		$itemList = D('Item')->where()->select();
+		$this->display('index');
+	}
 }
