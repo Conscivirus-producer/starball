affc720146d60ce8f7aadd9ca7df47f9f4eec18a
AnderLX <andrew1230@gmail.com>
2016-03-10 16:53:03 +0800

    item list controller

--- Application/Starball/Controller/ListController.class.php
+++ Application/Starball/Controller/ListController.class.php
@@ -6,8 +6,15 @@ class ListController extends Controller {
     	$this->display();
     }
 	
-	public function byTimeRange($range, $page=1){
-		$itemList = D('Item')->where()->select();
+	public function byTimeRange($range, $page=1, $filter=array()){
+		$itemList = D('Item')->field('t_item.*, img.imageSmall, img.imageMiddle, img.imageBig')
+							 ->where('DateDiff(now(),lastUpdatedDate)<='.$range)
+							 ->join('t_image img ON img.itemId = t_item.itemId')
+							 ->order('brandId desc,categoryId desc')
+							 ->page($page.',10')
+							 ->select();
+		logInfo(json_encode($itemList));
+		$this->assign('itemList',$itemList);
 		$this->display('index');
 	}
 }
