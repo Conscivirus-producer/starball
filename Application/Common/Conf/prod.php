24a4b393a26d1bf4343342ca21db454ca77c2977
Zheng Huahua <sven.zhenghh@gmail.com>
2016-03-28 18:59:09 +0800

    change schema to prod

--- Application/Common/Conf/prod.php
+++ Application/Common/Conf/prod.php
@@ -8,7 +8,7 @@ return array(
 	//数据库配置信息
 	'DB_TYPE'=>'mysql',// 数据库类型
 	'DB_HOST'=>'localhost',// 服务器地址
-	'DB_NAME'=>'starball_schema',// 数据库名
+	'DB_NAME'=>'test_schema',// 数据库名
 	'DB_USER'=>'root',// 用户名
 	'DB_PWD'=>'2324150778t',// 密码
 	'DB_PORT'=>3306,// 端口
