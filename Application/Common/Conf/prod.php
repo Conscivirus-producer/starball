d6d5fa28c5f83249ec577cc1af6fddd16b3a5d04
Zheng Huahua <sven.zhenghh@gmail.com>
2016-03-28 20:18:21 +0800

    push prod.php

--- Application/Common/Conf/prod.php
+++ Application/Common/Conf/prod.php
@@ -2,15 +2,13 @@
 return array(
 	//数据库配置信息
 	'IS_DEV'=>'false',// 本地开发时设成true,服务器上这项值 为'false'
-	'BUCKET'=>'video',//改成本地设置,开发为'videodev',产品环境为'video'
-	'QINIU_VIDEO_PREFIX'		  => 'http://7xoqga.com2.z0.glb.qiniucdn.com/', //七牛视频存储路径
 	
 	//数据库配置信息
 	'DB_TYPE'=>'mysql',// 数据库类型
 	'DB_HOST'=>'localhost',// 服务器地址
 	'DB_NAME'=>'test_schema',// 数据库名
 	'DB_USER'=>'root',// 用户名
-	'DB_PWD'=>'2324150778t',// 密码
+	'DB_PWD'=>'wang123456',// 密码
 	'DB_PORT'=>3306,// 端口
 	'DB_PREFIX'=>'',// 数据库表前缀
 	'DB_CHARSET'=>'utf8',// 数据库字符集
