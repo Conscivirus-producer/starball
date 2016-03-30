4128711f6c1fa5f967714b010c0f76cccc9f2c91
AnderLX <andrew1230@gmail.com>
2016-03-30 21:24:03 +0800

    menu js

--- Public/starball/js/home.js
+++ Public/starball/js/home.js
@@ -1,31 +1,26 @@
 var timeOut;
 $(function() {
-	$('.am-slider').flexslider({
-		// options
-		smoothHeight : false
-	});
+  $('.am-slider').flexslider({
+    // options
+    smoothHeight: false
+  });
 });
-$("a.yu-menu").mouseenter(function() {
-	$("div.yu-sub-menu").not("#" + $(this).attr("id")).fadeOut();
+$("a.yu-menu").mouseenter(function(){
+	$("div.yu-sub-menu").not("#"+$(this).attr("id")).fadeOut();
 	$(this).css("border-bottom", "4px solid black");
-	$("div.yu-sub-menu" + "." + $(this).attr("id")).fadeIn();
+	$("div.yu-sub-menu"+"."+$(this).attr("id")).fadeIn();
 	$("div.yu-sub-menu").css("height", $("div.am-viewport").css('height'));
 });
-$("a.yu-menu").mouseleave(function() {
-	
+$("a.yu-menu").mouseleave(function(){
 	var element = $(this).attr("id");
 	$(this).css("border-bottom", "");
-	timeOut = setTimeout(function() {
-		$("div.yu-sub-menu" + "." + element).fadeOut("");
+	timeOut = setTimeout(function(){
+		$("div.yu-sub-menu"+"."+element).fadeOut("");
 	}, 150);
 });
-$("div.yu-sub-menu.am-u-md-12").mouseenter(function() {
+$("div.yu-sub-menu.am-u-md-12").mouseenter(function(){
 	clearTimeout(timeOut);
 });
-$("div.yu-sub-menu.am-u-md-12").mouseleave(function() {
+$("div.yu-sub-menu.am-u-md-12").mouseleave(function(){
+	$(this).fadeOut();
 });
-$(function() {
-	$dropdown.on('open.dropdown.amui', function(e) {
-		console.log('open event triggered');
-	});
-}); 
