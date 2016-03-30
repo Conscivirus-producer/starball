b30c3cea1e086f47760e794e4e720e16b59441d8
Zheng Huahua <sven.zhenghh@gmail.com>
2016-03-30 18:23:54 +0800

    push merge loss

--- Application/Common/Logic/ItemLogic.class.php
+++ Application/Common/Logic/ItemLogic.class.php
@@ -27,6 +27,23 @@
 			}
  		}
 
+		public function getBrandNameListByGrade($grade, $gender){
+			$map['grade'] = $grade;
+			if($gender != ''){
+				$map['gender'] = $gender;
+			}
+			$data = $this->distinct(true)->field('t_brand.brandName, t_brand.brandId')->where($map)->join('t_brand on t_item.brandId = t_brand.brandId')->select();
+			return $data;
+		}
+		
+		public function getCategoryNameByGrade($grade, $gender){
+			$map['grade'] = $grade;
+			if($gender != ''){
+				$map['gender'] = $gender;
+			}
+			$data = $this->distinct(true)->field('t_item.categoryId, t_category.categoryName')->where($map)->join('t_category on t_item.categoryId = t_brand.categoryId')->select();
+			return $data;
+		}
 	}
 
 
