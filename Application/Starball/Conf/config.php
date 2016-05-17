e19e43fad54894f60a2698e603b52240bd39cb06
Zheng Huahua <sven.zhenghh@gmail.com>
2016-05-17 17:11:08 +0800

    add login by sinaweibo code

--- Application/Starball/Conf/config.php
+++ Application/Starball/Conf/config.php
@@ -1,6 +1,7 @@
 <?php
 return array(
 	//'配置项'=>'配置值'
+	'LOAD_EXT_CONFIG' => 'socialmedialogin',
 	'LANG_SWITCH_ON' => true,   // 开启语言包功能
 	'LANG_AUTO_DETECT' => true, // 自动侦测语言 开启多语言功能后有效
 	'LANG_LIST'        => 'zh-cn,zh-tw', // 允许切换的语言列表 用逗号分隔
--- Application/Starball/Controller/BaseController.class.php
+++ Application/Starball/Controller/BaseController.class.php
@@ -384,4 +384,61 @@ class BaseController extends Controller {
 		}
 		return $inadequateInventoryItems;
 	}
+	
+	public function loginFromSocialMedia($type = null){
+		empty($type) && $this->error('参数错误');
+
+		//加载ThinkOauth类并实例化一个对象
+		import("Org.ThinkSDK.ThinkOauth");
+		$sns  = \ThinkOauth::getInstance($type);
+
+		//跳转到授权页面
+		redirect($sns->getRequestCodeURL());
+	}
+	
+	//授权回调地址
+	public function callback($type = null, $code = null){
+		(empty($type) || empty($code)) && $this->error('参数错误');
+		
+		//加载ThinkOauth类并实例化一个对象
+		import("Org.ThinkSDK.ThinkOauth");
+		$sns  = \ThinkOauth::getInstance($type);
+
+		//腾讯微博需传递的额外参数
+		$extend = null;
+		if($type == 'tencent'){
+			$extend = array('openid' => $this->_get('openid'), 'openkey' => $this->_get('openkey'));
+		}
+
+		//请妥善保管这里获取到的Token信息，方便以后API调用
+		//调用方法，实例化SDK对象的时候直接作为构造函数的第二个参数传入
+		//如： $qq = ThinkOauth::getInstance('qq', $token);
+		$token = $sns->getAccessToken($code , $extend);
+
+		//获取当前登录用户信息
+		if(is_array($token)){
+			session('sina_token', $token);
+			$user_info = A('Type', 'Event')->$type($token);
+			$weiboId = $token['openid'];
+			$weiboName = $user_info['name'];
+			$this->redirect('Home/index');
+			//$this->checkExistingUserInformation($weiboId, $weiboName);
+		}
+	}
+	
+ 	//登录成功，微信用户信息
+    public function weixin($token){
+        $weixin   = \ThinkOauth::getInstance('weixin', $token);
+        $data = $weixin->call('sns/userinfo');
+
+        if($data['ret'] == 0){
+            $userInfo['type'] = 'WEIXIN';
+            $userInfo['name'] = $data['nickname'];
+            $userInfo['nick'] = $data['nickname'];
+            $userInfo['head'] = $data['headimgurl'];
+            return $userInfo;
+        } else {
+            throw_exception("获取微信用户信息失败：{$data['errmsg']}");
+        }
+    }
 }
