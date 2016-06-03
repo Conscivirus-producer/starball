5673c5db2d5bca511dcc6d36430647f3443e3290
Zheng Huahua <sven.zhenghh@gmail.com>
2016-06-03 22:24:05 +0800

    add to shopping list, ajax call to show the page

--- Application/Starball/Lang/zh-tw.php
+++ Application/Starball/Lang/zh-tw.php
@@ -23,7 +23,6 @@ return array(
     'commingsoon'=>'即将有货',
     'addToShoppingList'=>'添加到購物車',
     'addToFavoriteList'=>'添加到我的收藏',
-    'size'=>'大小',
     'recentShoppingList'=>'最近加入的宝贝',
     'totalAmount'=>'总金额',
     'buynow'=>'立即購買',
